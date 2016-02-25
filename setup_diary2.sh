4d2ee31439a88594b0b8d7003345ae9586aa5b7e
Shutaro Takimoto <shu@tak.im>
2016-02-25 17:49:47 +0900

    リポジトリ名変更に追従

--- setup_diary2.sh
+++ setup_diary2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 PG_USER='postgres'
 DB_NAME='diary2'
-GH_PAGES="http://progedu.github.io/rdb_study/"
+GH_PAGES="http://progedu.github.io/rdb-study/"
 SQL_DUMP="dump_${DB_NAME}.sql"
 
 sudo -u $PG_USER dropdb --if-exists "$DB_NAME"
